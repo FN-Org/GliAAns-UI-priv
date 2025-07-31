e9c39e5af0c302fe784b1a8783a52d269dfcd030
fedeg202 <s5187919@studenti.unige.it>
2025-07-31 10:03:19 +0200

    Update ui_import_frame.py

--- ui/ui_import_frame.py
+++ ui/ui_import_frame.py
@@ -14,7 +14,7 @@ import pydicom
 from wizard_controller import WizardPage
 
 class ImportThread(QThread):
-    finished = pyqtSignal()  
+    finished = pyqtSignal()
     error = pyqtSignal(str)
     progress = pyqtSignal(int)
 
