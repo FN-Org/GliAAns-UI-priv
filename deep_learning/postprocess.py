e22f0496726968137fa2c3d67cd7796272e7c5db
N1C03BN0 <s5182167@studenti.unige.it>
2025-10-30 08:57:56 +0100

    Little update

--- deep_learning/postprocess.py
+++ deep_learning/postprocess.py
@@ -10,7 +10,7 @@ import os
 from glob import glob
 from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser
 
-from deep_learning.utils.coreg import align, transform
+from utils.coreg import align, transform
 
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if PROJECT_ROOT not in sys.path:
